d61da795b69af241b7ce4273b9af0c03d641cb0e
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-22 23:38:08 +0800

    Set up home page components and styles

---
+++ src/pages/HomePage/components/carousel.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default class HomeCarousel extends React.Component {
+    public render() {
+        return (
+            <div className="carousel">
+                Something
+            </div>
+        );
+    }
+}
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,15 @@
-import * as React from 'react';
+import * as React from "react";
+import HomeCarousel from "./components/carousel";
+import HomeFooter from "./components/footer";
+import "./styles.css";
 
 export default class HomePage extends React.Component {
-    
+    public render() {
+        return (
+            <div>
+                <HomeCarousel />
+                <HomeFooter />
+            </div>
+        );
+    }
 }
