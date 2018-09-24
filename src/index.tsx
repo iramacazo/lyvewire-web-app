1337e7b5295a13dab52afa83c327f003b758343a
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-24 14:19:51 +0800

    Added provider to app root

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
+import { Provider } from "mobx-react";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./pages/App";
 import registerServiceWorker from "./registerServiceWorker";
+import rootStore from "./store";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+const app = (
+    <Provider {...rootStore}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
