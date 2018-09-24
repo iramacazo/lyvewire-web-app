20960b9c428919c6c15d850ddcd11ffcdb2f135e
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-24 19:35:59 +0800

    Format carousel item

--- src/pages/HomePage/components/CarouselItem/index.tsx
+++ src/pages/HomePage/components/CarouselItem/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Button } from "semantic-ui-react";
 import CarouselItem from "../../../../entities/carousel_item";
 
 interface IPropsType {
@@ -11,10 +12,18 @@ export default class CarouselItemView extends React.Component<IPropsType> {
             carouselItem: { imageUrl, eventSummary, eventTitle },
         } = this.props;
         return (
-            <div>
-                <img src={imageUrl} />
-                <h1>{eventTitle}</h1>
-                <p>{eventSummary}</p>
+            <div className="carousel">
+                <div className="event-cover">
+                    <div className="event-text">
+                        <h1 className="event-title">{eventTitle}</h1>
+                        <p>{eventSummary}</p>
+                    </div>
+                    <div className="event-buttons">
+                        <Button inverted={true} size="large">View Details</Button>
+                        <Button inverted={true} size="large">Watch Event</Button>
+                    </div>
+                </div>
+                <img src={imageUrl} className="carousel-image" />
             </div>
         );
     }
