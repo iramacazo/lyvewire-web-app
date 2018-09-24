05bac9995b5becb974ea2de4de2b66f824ede8e6
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-24 19:34:23 +0800

    Comment out landing page and arrange imports

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
-import LandingPage from "../LandingPage";
-import MenuBar from "./components/menu";
+// import LandingPage from "../LandingPage";
+import HomePage from "../HomePage";
+import MenuBar from "./components/Menu/menu";
 import "./styles.css";
 // import StreamingPage from "../StreamingPage";
 
 class App extends React.Component {
     public render() {
-        return (
-            <div className="App">
+        return <div className="App">
                 <MenuBar />
-                <LandingPage />
-            </div>
-        );
+                <HomePage />
+                {/* <LandingPage /> */}
+            </div>;
     }
 }
 
