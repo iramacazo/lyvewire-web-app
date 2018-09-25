c8a0074b718d798c1fa8acf180c14fb5a94e412e
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-25 14:20:16 +0800

    Import modal content and create toggle function

--- src/pages/App/components/UserModal/modal.tsx
+++ src/pages/App/components/UserModal/modal.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Modal } from "semantic-ui-react";
+import SignIn from "../SignIn/sign_in";
+import SignUp from "../SignUp/sign_up";
 
 interface IPropsType {
     isOpen: boolean;
@@ -7,13 +9,40 @@ interface IPropsType {
 }
 
 export default class UserModal extends React.Component<IPropsType> {
+    public state = {
+        activeContent: SignIn,
+    };
+
+    public toggleContent = () => {
+        switch (this.state.activeContent) {
+            case SignUp:
+                this.setState({
+                    activeContent: SignIn,
+                });
+                break;
+
+            default:
+                this.setState({
+                    activeContent: SignUp,
+                });
+        }
+    };
+
     public render() {
         return (
-            <div className="user-modal">
-                <Modal open={this.props.isOpen} size="small" onClose={this.props.toggleModal}>
-                    Something
-                </Modal>
-            </div>
+            <Modal
+                className="user-modal"
+                open={this.props.isOpen}
+                size="tiny"
+                onClose={this.props.toggleModal}
+            >
+                <Modal.Header>
+                    {this.state.activeContent === SignIn
+                        ? "Sign In"
+                        : "Sign Up"}
+                </Modal.Header>
+                <this.state.activeContent toggleContent={this.toggleContent} />
+            </Modal>
         );
     }
 }
