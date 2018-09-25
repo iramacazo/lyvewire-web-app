02ef8a929ce9c255c3a48b91a414a570503456f9
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-25 10:15:53 +0800

    Add modal, toggle state and function

--- src/pages/App/components/Menu/menu.tsx
+++ src/pages/App/components/Menu/menu.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import { Menu } from "semantic-ui-react";
+import UserModal from "../UserModal/modal";
 
 export default class MenuBar extends React.Component {
+    public state = {
+        isOpen: false,
+    };
+
+    public toggleModal = () => {
+        this.setState({
+            isOpen: !this.state.isOpen,
+        });
+    }
+
     public render() {
         return (
             <div className="menu-bar">
@@ -13,7 +24,16 @@ export default class MenuBar extends React.Component {
                 >
                     <Menu.Item className="menu-item">Lyvewire</Menu.Item>
                     <Menu.Menu position="right">
-                        <Menu.Item className="menu-item">Sign In</Menu.Item>
+                        <Menu.Item
+                            className="menu-item"
+                            onClick={this.toggleModal}
+                        >
+                            Sign In
+                        </Menu.Item>
+                        <UserModal
+                            isOpen={this.state.isOpen}
+                            toggleModal={this.toggleModal}
+                        />
                     </Menu.Menu>
                 </Menu>
             </div>
