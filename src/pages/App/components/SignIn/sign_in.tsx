5ac317c7bad42bbf0925af71e953b4de06942a55
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-25 16:21:34 +0800

    Add sign in form inputs and button

--- src/pages/App/components/SignIn/sign_in.tsx
+++ src/pages/App/components/SignIn/sign_in.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Modal } from "semantic-ui-react";
+import { Button, Container, Input, Modal } from "semantic-ui-react";
 
 interface IPropsType {
     toggleContent: any;
@@ -9,6 +9,34 @@ export default class SignIn extends React.Component<IPropsType> {
     public render() {
         return (
             <Modal.Content>
+                <Container>
+                    <div className="inputs">
+                        <div className="input-div">
+                            <h4>Email</h4>
+                            <Input
+                                icon="mail"
+                                iconPosition="left"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="input-div">
+                            <h4>Password</h4>
+                            <Input
+                                icon="asterisk"
+                                iconPosition="left"
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+                    <Button
+                        size="large"
+                        color="black"
+                        className="sign-button"
+                    >
+                        Sign In
+                    </Button>
+                </Container>
                 <p className="link" onClick={this.props.toggleContent}>
                     Create an account
                 </p>
