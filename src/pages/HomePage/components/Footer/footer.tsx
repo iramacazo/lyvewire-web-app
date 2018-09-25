2ce9392f39ca57c1918ced9a2b45366dbe70527b
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-09-25 17:42:39 +0800

    Add footer details

--- src/pages/HomePage/components/Footer/footer.tsx
+++ src/pages/HomePage/components/Footer/footer.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 export default class HomeFooter extends React.Component {
     public render() {
@@ -10,6 +10,18 @@ export default class HomeFooter extends React.Component {
                     <Button inverted={true}>Join our team</Button>
                     <Button inverted={true}>Go lyve</Button>
                     <Button inverted={true}>Submit feedback</Button>
+                </div>
+                <div className="footer-team">
+                    <div>
+                        <Icon inverted={true} name="react" size="large" />
+                        <Icon inverted={true} name="node" size="large" />
+                        <Icon inverted={true} name="npm" size="large" />
+                        <Icon inverted={true} name="github square" size="large" />
+                    </div>
+                    <p>
+                        Jason Deniega, Keith Tan, Oliver Arguelles, Ira Macazo,
+                        CL Pagaduan
+                    </p>
                 </div>
             </div>
         );
