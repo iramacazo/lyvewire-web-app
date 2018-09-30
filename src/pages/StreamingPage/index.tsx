94447daa9131ef79aa3164c3e02d7c343bbd14d3
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-10-01 00:03:29 +0800

    Import streaming page components

--- src/pages/StreamingPage/index.tsx
+++ src/pages/StreamingPage/index.tsx
@@ -1,5 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
+import StreamNavigation from "./components/Navigation";
+import Sidebar from "./components/Sidebar";
+import Stream from "./components/Stream";
+import "./styles.css";
 
 export default class StreamingPage extends React.Component {
-
+    public render() {
+        return (
+            <div className="stream-page">
+                <StreamNavigation />
+                <div className="stream-content">
+                    <Stream />
+                    <div className="stream-sidebar">
+                        <Sidebar />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
