96c12b996251da55f1c97b1b590ec735c3d95d3b
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-10-01 16:54:46 +0800

    Add image and divs to chat messages

--- src/pages/StreamingPage/components/StreamChat/components/ChatMessage/index.tsx
+++ src/pages/StreamingPage/components/StreamChat/components/ChatMessage/index.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
-import { Feed } from "semantic-ui-react";
+import { Feed, Image } from "semantic-ui-react";
+import chatIcon from "../../../../../../assets/images/speaker2.jpg";
 
 export default class ChatMessage extends React.Component {
     public render() {
+        const image = <Image src={chatIcon} circular={true} size="mini" />;
         return (
             <Feed.Event>
                 <Feed.Content>
-                    <Feed.Summary>
-                        <a>Keith Tan</a>
-                    </Feed.Summary>
+                    <div className="chat-message-header">
+                        <Feed.Label image={image} />
+                        <div className="chat-message-details">
+                            <Feed.Summary>
+                                <a>Keith Tan</a>
+                                <Feed.Date>2 seconds ago</Feed.Date>
+                            </Feed.Summary>
+                        </div>
+                    </div>
                     <Feed.Extra>
                         Here's a long test message. Loren ipsum dolor est
                         something idk look I need professional mental help. If
