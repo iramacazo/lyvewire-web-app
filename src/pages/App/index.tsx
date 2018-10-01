c5be674e400bb7ec9649367cce33bc5155bcc70f
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-10-01 14:03:26 +0800

    Change display page to streaming page

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
-import LandingPage from "../LandingPage";
+// import LandingPage from "../LandingPage";
+import StreamingPage from "../StreamingPage";
 // import HomePage from "../HomePage";
-import MenuBar from "./components/Menu/menu";
+// import MenuBar from "./components/Menu/menu";
 import "./styles.css";
 // import StreamingPage from "../StreamingPage";
 
 class App extends React.Component {
     public render() {
         return <div className="App">
-                <MenuBar />
+                {/* <MenuBar /> */}
                 {/* <HomePage /> */}
-                <LandingPage />
+                {/* <LandingPage /> */}
+                <StreamingPage />
             </div>;
     }
 }
