de6794ab95997d93b7f9c50c9b909d63dffe5efc
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-10-01 14:05:39 +0800

    Set up questions sidebar section

--- src/pages/StreamingPage/components/StreamQuestions/index.tsx
+++ src/pages/StreamingPage/components/StreamQuestions/index.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 
 interface IPropsType {
     expanded: boolean;
+    toggleActive: any;
 }
 
 export default class StreamQuestions extends React.Component<IPropsType> {
     public render() {
         return (
-            <div>
-                Something
+            <div className="stream-questions" onClick={this.props.toggleActive}>
+                <h3>Questions</h3>
             </div>
         );
     }
