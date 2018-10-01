5f6a25175af9009f345e28b8d9e13d5fb51eca43
hideyarguelles <catherine_arguelles@dlsu.edu.ph>
2018-10-01 14:05:24 +0800

    Add dummy stream chat layout

--- src/pages/StreamingPage/components/StreamChat/index.tsx
+++ src/pages/StreamingPage/components/StreamChat/index.tsx
@@ -1,14 +1,40 @@
 import * as React from "react";
+import { Feed, Input } from "semantic-ui-react";
+import ChatMessage from "./components/ChatMessage";
 
 interface IPropsType {
     expanded: boolean;
+    toggleActive: any;
 }
 
 export default class StreamChat extends React.Component<IPropsType> {
     public render() {
         return (
-            <div>
-                Something
+            <div className="stream-chat" onClick={this.props.toggleActive}>
+                {!this.props.expanded && (
+                    <React.Fragment>
+                        <h3>Chat</h3>
+                    </React.Fragment>
+                )}
+
+                {this.props.expanded && (
+                    <React.Fragment>
+                        <h3>Chat</h3>
+                        <Feed>
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                            <ChatMessage />
+                        </Feed>
+                        <div className="chat-input">
+                            <Input placeholder="Type in chat..." />
+                        </div>
+                    </React.Fragment>
+                )}
             </div>
         );
     }
